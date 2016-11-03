d6e274378e9ebe606595d60a5032edeeeecd6adb
a38481 <tiagocruz93@hotmail.com>
2016-11-03 21:04:38 +0000

    About Activiy toolbar

--- app/src/main/java/com/example/workstation/pdm_se01/AboutActivity.java
+++ app/src/main/java/com/example/workstation/pdm_se01/AboutActivity.java
@@ -1,20 +1,21 @@
 package com.example.workstation.pdm_se01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
-    TextView helptext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aboutus);
-
-        
-
-
-
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
+
 }
