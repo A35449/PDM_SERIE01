42c5da9c9a3de5c782f4d5a129a10a1111a993fc
a38481 <tiagocruz93@hotmail.com>
2016-11-03 20:53:45 +0000

    Aboutus

---
+++ app/src/main/java/com/example/workstation/pdm_se01/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.workstation.pdm_se01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+    TextView helptext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+
+        
+
+
+
+
+    }
+}
