5c00528f4ec09be48f82a887819cb76bf3a3cf26
a38481 <tiagocruz93@hotmail.com>
2016-12-14 20:33:49 +0000

    HomeActivity and PreferencesActivity

---
+++ app/src/main/java/com/example/workstation/pdm_se01/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workstation.pdm_se01.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
