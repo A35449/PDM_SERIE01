d2d0a848d292bd1c7b9107da105f2eadcd2612fb
Presumably <joseflopez92@gmail.com>
2016-12-15 21:07:25 +0000

    New changelist

--- app/src/main/java/com/example/workstation/pdm_se01/components/broadcast/AWAReceiver.kt
+++ app/src/main/java/com/example/workstation/pdm_se01/components/broadcast/AWAReceiver.kt
@@ -4,11 +4,14 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.widget.Toast
 import android.os.BatteryManager
-
-
+import android.os.SystemClock
+import com.example.workstation.pdm_se01.AWApplication
+import com.example.workstation.pdm_se01.alarmIntent
+import com.example.workstation.pdm_se01.alarmMgr
 
 /**
  * Created by Jos on 13/12/2016.
@@ -23,17 +26,36 @@ class AWAReceiver : BroadcastReceiver() {
                 val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
                 val batteryPct = level / scale.toFloat()
                 Toast.makeText(context, "Battery :  " +batteryPct, Toast.LENGTH_SHORT).show()
-            }
-            Intent.ACTION_BATTERY_LOW -> {
-                val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                val cancelPIntent : PendingIntent
-                val intentCancel = Intent(context, AWAReceiver::class.java)
-                cancelPIntent = PendingIntent.getBroadcast(context,0,intentCancel,0)
-                alarmManager.cancel(cancelPIntent)
-                Toast.makeText(context, "Battery LOW ", Toast.LENGTH_SHORT).show()
-            }
-            Intent.ACTION_BATTERY_OKAY -> {
-                Toast.makeText(context, "Battery OKAY", Toast.LENGTH_SHORT).show()
+                val shared=context?.getSharedPreferences("alarmSet",MODE_PRIVATE)
+                val alarmSet = shared?.getBoolean("alarmSet",true) as Boolean
+
+                if(batteryPct<0.5 && alarmSet){
+                    val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                    val cancelPIntent : PendingIntent
+                    val intentCancel = Intent(context, AWAReceiver::class.java)
+                    cancelPIntent = PendingIntent.getBroadcast(context,0,intentCancel,0)
+                    alarmManager.cancel(cancelPIntent)
+                    Toast.makeText(context, "Battery lower then settings allow ,alarm unset", Toast.LENGTH_SHORT).show()
+
+                    val shared= context?.getSharedPreferences("alarmSet", Context.MODE_PRIVATE)
+                    val editor= shared?.edit()
+                    editor?.putBoolean("alarmSet",false)
+                    editor?.commit()
+
+                }
+                else if (batteryPct>=0.5 && !alarmSet){
+                    alarmMgr?.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime(),
+                            6000, alarmIntent)
+
+                    val shared= context?.getSharedPreferences("alarmSet", Context.MODE_PRIVATE)
+                    val editor= shared?.edit()
+                    editor?.putBoolean("alarmSet",true)
+                    editor?.commit()
+                    Toast.makeText(context, "Battery higher then settings allow ,alarm Set", Toast.LENGTH_SHORT).show()
+
+                }
+                else  Toast.makeText(context, "No changes done to alarm", Toast.LENGTH_SHORT).show()
 
             }
             Intent.ACTION_BOOT_COMPLETED -> {
