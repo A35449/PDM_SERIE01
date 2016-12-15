b677304a75650eab592c8296b95fa9c9f269ab23
Presumably <joseflopez92@gmail.com>
2016-12-15 22:20:01 +0000

    New changelist

--- app/src/main/java/com/example/workstation/pdm_se01/components/broadcast/UpdateService.kt
+++ app/src/main/java/com/example/workstation/pdm_se01/components/broadcast/UpdateService.kt
@@ -2,20 +2,16 @@ package com.example.workstation.pdm_se01.components.broadcast
 
 import android.app.*
 import android.content.Intent
-import android.os.IBinder
-import android.widget.Toast
-import android.os.Binder
-import android.util.Log
-import com.example.workstation.pdm_se01.R
-import com.example.workstation.pdm_se01.activities.WeatherActivity
+import com.example.workstation.pdm_se01.utils.Utils
 
 
 /**
  * Created by Jos on 14/12/2016.
  */
 class UpdateService : IntentService("UpdateService") {
-    override fun onHandleIntent(intent: Intent?) {
-        Log.d("ttt","ttt")
-    }
 
-}
+        override fun onHandleIntent(intent: Intent?) {
+            Utils.sendNotification(applicationContext,"Update Started","Update Started","")
+        }
+
+    }
