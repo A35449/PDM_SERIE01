fa8fb04d60c7e9f5faae6cf7655898e6f0630b5e
a38481 <tiagocruz93@hotmail.com>
2016-12-15 19:55:04 +0000

    preferences activity design

--- app/src/main/java/com/example/workstation/pdm_se01/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/workstation/pdm_se01/activities/SettingsActivity.kt
@@ -1,17 +1,26 @@
 package com.example.workstation.pdm_se01.activities
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
 
 import com.example.workstation.pdm_se01.R
 
 class SettingsActivity : AppCompatActivity() {
+    private var sharedPrefLocation: SharedPreferences? = null
+    private var applyButton:Button?=null
+    private var aboutUsButton:Button?=null
+    private var seekbar:SeekBar?=null
+    private var editText:EditText?=null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
